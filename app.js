bf388b1e4ead9f7e30c7dee87d4f1bf2c0b39832
Sooparkle <105525761+Sooparkle@users.noreply.github.com>
2024-03-03 21:51:43 +0900

    Update app.js
    - add Supabase logic

--- app.js
+++ app.js
@@ -1,74 +1,128 @@
 const express = require('express');
+const fetch = require("node-fetch");
 const bodyParaser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 4000;
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
 
 app.use(cors());
 app.use(bodyParaser.json());
 
 
+//사용자 테스트 페이지
 app.get('/', async(req, res)=>{
   if(res.status(200)){
     res.send('<h1>데이터 통신 성공</h>');
   } else {
     res.status(404).send('서버 오류')
   }
-
 })
 
+// Supabase
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 
-app.post('/navebooks', async(req, res)=>{
+// 네이버 Social Media 로그인 
+const client_id = process.env.NODE_NAVER_API_ID;
+const client_secret = process.env.NODE_NAVER_API_SECRET;
+const state = "RANDOM_STATE-anyword";
+const redirectURI = encodeURI("http://localhost:3000/callback");
+const api_url = "";
 
 
-  try {
-    const title = req.body.book
-    const images =[]
-    for (const book of Object.values(title)){
 
 
-  const naverUrl = new URL('https://openapi.naver.com/v1/search/book.json');
-  naverUrl.searchParams.set('query', book);
-  naverUrl.searchParams.set('display', 1);
-
-  const options ={
-    method : 'GET',
-    headers : {
-      'Content-Type': 'application/json',
-      'X-Naver-Client-Id': 'ytaQUUoLOhwBVF3BCR1m',
-      'X-Naver-Client-Secret': 'FQqOQ31UUj'
-    }
-  };
-  const response = await fetch(naverUrl.toString(), options);
-
-  // console.log("FetchBookImage function", response);
-
-  if(!response.ok){
-    throw new Error (`Naver API fetch failed ${response.status}`)
-  }
-
-  const data = await response.json();
-
-  // console.log("Response URL", data.items[0]);
-
-  images.push({
-    title : data.items[0].title,
-    imageURL : data.items[0].image,
-    description : data.items[0].description
-  })
-}
-  res.json(images);
-// console.log("BookUrl List1", images)
+// accomudation data fetch from Supabase
+app.get('/accommodation', async (req, res) =>{
   
-} 
+  try{
+    const { data, error } = await supabase
+    .from('accoms')
+    .select('*');
+    if (error) {
+      throw error;
+    }
+    
+    console.log("작동중", data);
+    res.json(data);
 
-catch(error) {
-  console.log("Error fetching data at Node", error)
-};
-})
+  } catch(error) {
+    res.status(500).json({ error : error.message });
+  }
+});
 
+
+
+app.get("/callback", async function (req, res) {
+  const code = req.query.code;
+  const state = req.query.state;
+  const redirectURI = "http://localhost:3000/login";
+  const api_url =
+    "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=" +
+    client_id +
+    "&client_secret=" +
+    client_secret +
+    "&redirect_uri=" +
+    redirectURI +
+    "&code=" +
+    code +
+    "&state=" +
+    state;
+
+    const response = await fetch(api_url, {
+      headers: {
+        "X-Naver-Client-Id": client_id,
+        "X-Naver-Client-Secret": client_secret,
+      },
+    });
+  
+    const tokenRequest = await response.json();
+    
+    if ("access_token" in tokenRequest) {
+      const { access_token } = tokenRequest;
+      const apiUrl = "https://openapi.naver.com/v1/nid/me";
+  
+      const data = await fetch(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      });
+  
+      const userData = await data.json();
+      const authData ={
+        name : userData.response.name,
+        email : userData.response.email
+      }
+    // try{
+    //   await supabaseClient
+    //   .from('users') // Your table name
+    //   .upsert({
+    //     email: authData.email,
+    //     name: authData.name,
+    //   })
+    //   .eq('email', authData.email) // Condition for merging existing data
+    //   .execute();
+
+
+    // console.log('User data saved to Supabase successfully!');
+    // res.redirect('/your-landing-page'); // Redirect to landing page
+    
+    // } catch{
+
+    //   console.error('Error saving user data to Supabase:', error);
+    //   res.status(500).send('Error saving data');
+
+    // }
+    }
+  
+    return res.send("DB에 저장하고 랜드페이지로 redirect ");
+  });
 
 
 app.listen(port, ()=>{
